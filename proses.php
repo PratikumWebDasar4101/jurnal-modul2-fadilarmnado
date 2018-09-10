fc54f13ea821da2f535d96001c841c675217a745
fadilarmnado <42725259+fadilarmnado@users.noreply.github.com>
2018-09-10 10:02:47 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,18 @@
 <?php 
 //Tuliskan kode anda di bawah ini
+	# code...
+	$angka = $_GET['angka'];
+	for ($i=$angka; $i >= 1 ; $i--) { 
+		# code...
+		for ($j=$i; $j >= 1 ; $j--) { 
+			
+			echo $j;
+		}
+	echo "<br>";
 
-?>
+	if (condition) {
+		# code...
+	}
+}
+
+ ?> 
